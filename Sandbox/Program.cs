f9180f29e055d547d3cccccc40a61e1859b16da7
Ricardo Rossi <solucoessw@gmail.com>
2018-12-13 21:41:23 -0200

    Teste criação de lista com o nome dos componentes

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using SldWorks;
+using SwConst;
+using static System.Console;
 
 namespace Sandbox
 {
@@ -10,6 +14,41 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
+            SldWorks.SldWorks swApp;
+            ModelDoc2 swModel;
+            AssemblyDoc swAsm;
+            Component2 swComp;
+            Feature swFeat;
+
+            swApp = (SldWorks.SldWorks)Marshal.GetActiveObject("SldWorks.Application");
+            swModel = swApp.ActiveDoc;
+
+            swAsm = (AssemblyDoc)swModel;
+
+            Object[] compcomponentes = swAsm.GetComponents(false);
+
+            var totalComp = compcomponentes.Length;
+            var listaNomesComInstancias =new List<string>();
+
+            foreach (var comp in compcomponentes)
+            {
+                swComp = (Component2)comp;
+                listaNomesComInstancias.Add(swComp.Name2);                
+            }
+
+            foreach (var nome in listaNomesComInstancias)
+            {
+                Component2 compRet = swAsm.GetComponentByName(nome);
+                swFeat = compRet.FeatureByName("cs_1");
+                if (swFeat!=null)
+                {
+                    swFeat.Select(true);
+
+
+                }
+                //compRet.Select(false);
+            }
+            ReadKey();
         }
     }
 }
