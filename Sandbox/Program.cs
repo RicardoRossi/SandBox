5a9afa960493a814cdb4ffea50b0b75338d71b2f
Ricardo Rossi <solucoessw@gmail.com>
2019-02-12 21:43:53 -0200

    Removida a matriz de coordenadas

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -18,7 +18,6 @@ namespace Sandbox
             ModelDoc2 swModel;
             AssemblyDoc swAsm;
             Component2 swComp;
-            Feature swFeat;
 
             swApp = (SldWorks.SldWorks)Marshal.GetActiveObject("SldWorks.Application");
             swModel = swApp.ActiveDoc;
@@ -42,40 +41,7 @@ namespace Sandbox
             object vcompXforms;
             object vcompCoordSysNames;
             object vcomponents;
-
-
-            //compNames[0] = @"C:\Users\RICARDO.000\Documents\_Projetos teste powerPDM\TORNO 2700 2008\53015012.sldprt";
-            //compNames[1] = @"C:\Users\RICARDO.000\Documents\_Projetos teste powerPDM\TORNO 2700 2008\53015012.sldprt";
-
-            // Define the transformation matrix. See the IMathTransform API documentation.
-            // Add a rotational diagonal unit matrix (zero rotation) to the transform
-            // x-axis components of rotation
-            compXforms[0] = 1.0;
-            compXforms[1] = 0.0;
-            compXforms[2] = 0.0;
-            // y-axis components of rotation
-            compXforms[3] = 0.0;
-            compXforms[4] = 1.0;
-            compXforms[5] = 0.0;
-            // z-axis components of rotation
-            compXforms[6] = 0.0;
-            compXforms[7] = 0.0;
-            compXforms[8] = 1.0;
-
-            // Add a translation vector to the transform (zero translation) 
-            compXforms[9] = 0.0;
-            compXforms[10] = 0.0;
-            compXforms[11] = 0.0;
-
-            // Add a scaling factor to the transform
-            compXforms[12] = 0.0;
-
-            // The last three elements in the transformation matrix are unused
-
-            // Register the coordinate system name for the component 
-            //compCoordSysNames[0] = "";
-            //compCoordSysNames[1] = "";
-
+                       
             // Add the components to the assembly. 
             vcompNames = compNames;
             vcompXforms = compXforms;
